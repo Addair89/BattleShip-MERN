977c9b052ee94b9e7c229f0f0601d4e6c448c917
Jared Addair <addairjared@gmail.com>
2024-04-23 06:06:36 -0700

    Adding api today"

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,9 +1,27 @@
+import NavBar from "../../components/NavBar/NavBar";
+import AuthPage from "../AuthPage/AuthPage";
+import NewOrderPage from "../NewOrderPage/NewOrderPage";
+import OrderHistoryPage from "../OrderHistoryPage/OrderHistoryPage";
 import "./App.css";
+import { useState } from "react";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
+  const [user, setUser] = useState(null);
+
   return (
     <main className="App">
-      <h1>App</h1>
+      {user ? (
+        <>
+          <NavBar />
+          <Routes>
+            <Route path="/orders/new" element={<NewOrderPage />} />
+            <Route path="/orders" element={<OrderHistoryPage />} />
+          </Routes>
+        </>
+      ) : (
+        <AuthPage />
+      )}
     </main>
   );
 }
---
+++ src/pages/AuthPage/AuthPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import SignUpForm from "../../components/SignUpForm/SignUpForm";
+
+const AuthPage = () => {
+  return (
+    <main>
+      <h1>AuthPage</h1>
+      <SignUpForm />
+    </main>
+  );
+};
+
+export default AuthPage;
